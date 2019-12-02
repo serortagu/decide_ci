4eb033905e068d7205156661dcb745e180900c12
Sergio Ortiz <serortagu@alum.us.es>
2019-12-02 15:35:04 +0000

    integracion continua api para que travis despliegue en heroku

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -72,7 +72,7 @@ MODULES = [
 ]
 
 #BASEURL = 'http://localhost:8000'
-BASEURL = 'http://heroku-meet-travis.herokuapp.com'
+BASEURL = 'http://decide-ci-serortagu.herokuapp.com'
 
 APIS = {
     'authentication': BASEURL,
